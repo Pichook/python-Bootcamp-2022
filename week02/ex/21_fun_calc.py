148bc325a65222d7dc3f97afb22b573694e138b5
unknown <pichouk577@gmail.com>
2022-07-04 21:07:43 +0700

    Added

---
+++ week02/ex/21_fun_calc.py
@@ -0,0 +1,7 @@
+# Write a function in python fun_calc(num1, num2, operator) that receives 3 arguments
+# (num1, num2 and operator). The first two arguments are operands (integers) and the third
+# argument (character) that receives symbols of operators (+ (or) – (or) * (or) /). The function
+# should calculate and return the result to the caller and the type of operation must be
+# defined with the third argument. The function should also print the result and also the
+# calculation process.
+
