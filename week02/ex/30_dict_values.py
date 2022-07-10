93442351b266c55d4be9761f71b921e8631167ee
unknown <pichouk577@gmail.com>
2022-07-10 20:48:14 +0700

    Added

--- week02/ex/30_dict_values.py
+++ week02/ex/30_dict_values.py
@@ -1,10 +1,25 @@
 
+my_dict = {120: ("Visal", "Borey", "Sovann"), 130: ("Hout","Mouyleng","Pidor"), 140: ("Nary", "Misora", "Masaki")}
+
+val = my_dict.values()
+x = tuple(val)
+
+def join_tuple_string(dict) -> str:
+   return ' '.join(dict)
+
+# joining all the tuples
+result = map(join_tuple_string, x)
+
+# converting and printing the result
+val_list = list(result)
+print(val_list)
+
 def dict_values(dict):
-    val = dict.
+   key = dict.keys()
+   key_list = list(key)
+   for i in range(0, len(dict)):
+      print(key_list[i], ':', val_list[i])
+      if i < len(dict)-1:
+         print("*****")
 
-
-
-
-
-my_dic = {120: ("Visal", "Borey", "Sovann"), 130: ("Hout","Mouyleng","Pidor"), 140: ("Nary", "Misora", "Masaki")}
-dict_values(my_dic)
+dict_values(my_dict)
